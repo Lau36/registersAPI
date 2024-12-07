5a4ef8b0d8bda56fe8f2d8e725bad902b41bdb35
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-12-07 16:34:01 -0500

    first commit

---
+++ src/main/java/com/example/registers_api/RegistersApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.registers_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistersApiApplication.class, args);
+	}
+
+}
